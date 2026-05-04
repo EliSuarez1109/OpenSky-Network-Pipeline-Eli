f01a9ea25e4d864b8dd06bb150b504f5f24fd630
EliSuarez1109 <amanemisa1109@gmail.com>
2026-05-04 09:44:32 +0200

    Logs segunda y tercera carga

--- opensky_eli/logs/query_log.sql
+++ opensky_eli/logs/query_log.sql
@@ -1,49 +1,38 @@
--- created_at: 2026-05-03T21:55:42.347309760+00:00
--- finished_at: 2026-05-03T21:55:42.657834277+00:00
--- elapsed: 310ms
--- outcome: error
--- error vendor code: 2043
--- error message: Internal: [Snowflake] 002043 (02000): SQL compilation error:
-Object does not exist, or operation cannot be performed.
--- dialect: snowflake
--- node_id: not available
--- query_id: not available
--- desc: list_relations_in_parallel
-SHOW OBJECTS IN SCHEMA "OPENSKY_DB"."RAW_MARTS" LIMIT 10000;
--- created_at: 2026-05-03T21:55:43.457498970+00:00
--- finished_at: 2026-05-03T21:55:43.916624362+00:00
--- elapsed: 459ms
+-- created_at: 2026-05-04T07:26:41.455987005+00:00
+-- finished_at: 2026-05-04T07:26:41.828325445+00:00
+-- elapsed: 372ms
 -- outcome: success
 -- dialect: snowflake
 -- node_id: not available
--- query_id: 01c42123-0108-bc7e-0029-56e70015d84e
+-- query_id: 01c4235e-0108-c1f1-0029-56e7001755f6
+-- desc: list_relations_in_parallel
+SHOW OBJECTS IN SCHEMA "OPENSKY_DB"."RAW_MARTS" LIMIT 10000;
+-- created_at: 2026-05-04T07:26:42.559882817+00:00
+-- finished_at: 2026-05-04T07:26:42.900005003+00:00
+-- elapsed: 340ms
+-- outcome: success
+-- dialect: snowflake
+-- node_id: not available
+-- query_id: 01c4235e-0108-c1f9-0029-56e7001775f6
 -- desc: execute adapter call
 show terse schemas in database OPENSKY_DB
     limit 10000
 /* {"app": "dbt", "connection_name": "", "dbt_version": "2.0.0", "profile_name": "opensky_eli", "target_name": "dev"} */;
--- created_at: 2026-05-03T21:55:43.929537238+00:00
--- finished_at: 2026-05-03T21:55:44.296314017+00:00
--- elapsed: 366ms
--- outcome: success
--- dialect: snowflake
--- node_id: not available
--- query_id: 01c42123-0108-bc62-0029-56e7001585e6
--- desc: execute adapter call
-create schema if not exists OPENSKY_DB.RAW_marts
-/* {"app": "dbt", "connection_name": "", "dbt_version": "2.0.0", "profile_name": "opensky_eli", "target_name": "dev"} */;
--- created_at: 2026-05-03T21:55:46.070263577+00:00
--- finished_at: 2026-05-03T21:55:47.266658576+00:00
--- elapsed: 1.2s
+-- created_at: 2026-05-04T07:26:44.124201244+00:00
+-- finished_at: 2026-05-04T07:26:44.850203192+00:00
+-- elapsed: 726ms
 -- outcome: success
 -- dialect: snowflake
 -- node_id: model.opensky_eli.fact_aircraft_incremental
--- query_id: 01c42123-0108-bc83-0029-56e70015c512
+-- query_id: 01c4235e-0108-bc85-0029-56e70015e612
 -- desc: execute adapter call
-create or replace transient  table OPENSKY_DB.RAW_marts.fact_aircraft_incremental
+create or replace  temporary view OPENSKY_DB.RAW_marts.fact_aircraft_incremental__dbt_tmp
+  
+  
+  
+  
+  as (
     
-    
-    
-    as (
 
 WITH raw_incremental AS (
     SELECT 
@@ -59,19 +48,120 @@ WITH raw_incremental AS (
     FROM OPENSKY_DB.RAW.aircraft_incremental_landing
 
     
+    -- La magia incremental: solo trae registros cargados después del último proceso
+    WHERE _loaded_at > (SELECT MAX(_loaded_at) FROM OPENSKY_DB.RAW_marts.fact_aircraft_incremental)
+    
 )
 
 SELECT * FROM raw_incremental
-    )
-
+  )
 /* {"app": "dbt", "dbt_version": "2.0.0", "node_id": "model.opensky_eli.fact_aircraft_incremental", "profile_name": "opensky_eli", "target_name": "dev"} */;
--- created_at: 2026-05-03T21:55:47.271699674+00:00
--- finished_at: 2026-05-03T21:55:47.578957311+00:00
--- elapsed: 307ms
+-- created_at: 2026-05-04T07:26:44.853790310+00:00
+-- finished_at: 2026-05-04T07:26:45.119413687+00:00
+-- elapsed: 265ms
 -- outcome: success
 -- dialect: snowflake
 -- node_id: model.opensky_eli.fact_aircraft_incremental
--- query_id: 01c42123-0108-baee-0029-56e70015479e
+-- query_id: 01c4235e-0108-c1f9-0029-56e7001775fa
+-- desc: execute adapter call
+describe table OPENSKY_DB.RAW_marts.fact_aircraft_incremental__dbt_tmp
+/* {"app": "dbt", "dbt_version": "2.0.0", "node_id": "model.opensky_eli.fact_aircraft_incremental", "profile_name": "opensky_eli", "target_name": "dev"} */;
+-- created_at: 2026-05-04T07:26:45.124520864+00:00
+-- finished_at: 2026-05-04T07:26:45.420601165+00:00
+-- elapsed: 296ms
+-- outcome: success
+-- dialect: snowflake
+-- node_id: model.opensky_eli.fact_aircraft_incremental
+-- query_id: 01c4235e-0108-c23b-0029-56e70017b612
+-- desc: execute adapter call
+describe table OPENSKY_DB.RAW_marts.fact_aircraft_incremental
+/* {"app": "dbt", "dbt_version": "2.0.0", "node_id": "model.opensky_eli.fact_aircraft_incremental", "profile_name": "opensky_eli", "target_name": "dev"} */;
+-- created_at: 2026-05-04T07:26:45.424012412+00:00
+-- finished_at: 2026-05-04T07:26:45.704140453+00:00
+-- elapsed: 280ms
+-- outcome: success
+-- dialect: snowflake
+-- node_id: model.opensky_eli.fact_aircraft_incremental
+-- query_id: 01c4235e-0108-bc7e-0029-56e70015d936
+-- desc: execute adapter call
+describe table OPENSKY_DB.RAW_marts.fact_aircraft_incremental__dbt_tmp
+/* {"app": "dbt", "dbt_version": "2.0.0", "node_id": "model.opensky_eli.fact_aircraft_incremental", "profile_name": "opensky_eli", "target_name": "dev"} */;
+-- created_at: 2026-05-04T07:26:45.707270604+00:00
+-- finished_at: 2026-05-04T07:26:45.970598722+00:00
+-- elapsed: 263ms
+-- outcome: success
+-- dialect: snowflake
+-- node_id: model.opensky_eli.fact_aircraft_incremental
+-- query_id: 01c4235e-0108-bc83-0029-56e70015c5fa
+-- desc: execute adapter call
+describe table "OPENSKY_DB"."RAW_MARTS"."FACT_AIRCRAFT_INCREMENTAL"
+/* {"app": "dbt", "dbt_version": "2.0.0", "node_id": "model.opensky_eli.fact_aircraft_incremental", "profile_name": "opensky_eli", "target_name": "dev"} */;
+-- created_at: 2026-05-04T07:26:45.982879489+00:00
+-- finished_at: 2026-05-04T07:26:46.252098966+00:00
+-- elapsed: 269ms
+-- outcome: success
+-- dialect: snowflake
+-- node_id: model.opensky_eli.fact_aircraft_incremental
+-- query_id: 01c4235e-0108-bc08-0029-56e7001596be
+-- desc: execute adapter call
+-- back compat for old kwarg name
+  
+  begin
+/* {"app": "dbt", "dbt_version": "2.0.0", "node_id": "model.opensky_eli.fact_aircraft_incremental", "profile_name": "opensky_eli", "target_name": "dev"} */;
+-- created_at: 2026-05-04T07:26:46.252510047+00:00
+-- finished_at: 2026-05-04T07:26:48.167890239+00:00
+-- elapsed: 1.9s
+-- outcome: success
+-- dialect: snowflake
+-- node_id: model.opensky_eli.fact_aircraft_incremental
+-- query_id: 01c4235e-0108-bbba-0029-56e7001577a2
+-- desc: execute adapter call
+
+    
+        
+            
+	    
+	    
+            
+        
+    
+
+    
+
+    merge into OPENSKY_DB.RAW_marts.fact_aircraft_incremental as DBT_INTERNAL_DEST
+        using OPENSKY_DB.RAW_marts.fact_aircraft_incremental__dbt_tmp as DBT_INTERNAL_SOURCE
+        on ((DBT_INTERNAL_SOURCE.icao24 = DBT_INTERNAL_DEST.icao24))
+
+    
+    when matched then update set
+        "ICAO24" = DBT_INTERNAL_SOURCE."ICAO24","REGISTRATION" = DBT_INTERNAL_SOURCE."REGISTRATION","MANUFACTURER" = DBT_INTERNAL_SOURCE."MANUFACTURER","MODEL" = DBT_INTERNAL_SOURCE."MODEL","OPERATOR" = DBT_INTERNAL_SOURCE."OPERATOR","AWS_LOAD_DATE" = DBT_INTERNAL_SOURCE."AWS_LOAD_DATE","_LOADED_AT" = DBT_INTERNAL_SOURCE."_LOADED_AT"
+    
+
+    when not matched then insert
+        ("ICAO24", "REGISTRATION", "MANUFACTURER", "MODEL", "OPERATOR", "AWS_LOAD_DATE", "_LOADED_AT")
+    values
+        ("ICAO24", "REGISTRATION", "MANUFACTURER", "MODEL", "OPERATOR", "AWS_LOAD_DATE", "_LOADED_AT")
+
+
+/* {"app": "dbt", "dbt_version": "2.0.0", "node_id": "model.opensky_eli.fact_aircraft_incremental", "profile_name": "opensky_eli", "target_name": "dev"} */;
+-- created_at: 2026-05-04T07:26:48.168235227+00:00
+-- finished_at: 2026-05-04T07:26:48.837283749+00:00
+-- elapsed: 669ms
+-- outcome: success
+-- dialect: snowflake
+-- node_id: model.opensky_eli.fact_aircraft_incremental
+-- query_id: 01c4235e-0108-c23b-0029-56e70017b616
+-- desc: execute adapter call
+
+    commit
+/* {"app": "dbt", "dbt_version": "2.0.0", "node_id": "model.opensky_eli.fact_aircraft_incremental", "profile_name": "opensky_eli", "target_name": "dev"} */;
+-- created_at: 2026-05-04T07:26:48.841463195+00:00
+-- finished_at: 2026-05-04T07:26:49.497446246+00:00
+-- elapsed: 655ms
+-- outcome: success
+-- dialect: snowflake
+-- node_id: model.opensky_eli.fact_aircraft_incremental
+-- query_id: 01c4235e-0108-bc85-0029-56e70015e616
 -- desc: execute adapter call
 drop view if exists OPENSKY_DB.RAW_marts.fact_aircraft_incremental__dbt_tmp cascade
 /* {"app": "dbt", "dbt_version": "2.0.0", "node_id": "model.opensky_eli.fact_aircraft_incremental", "profile_name": "opensky_eli", "target_name": "dev"} */;
